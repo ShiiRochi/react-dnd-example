66692eb021e5795d41ea15ddf6525046ff10dcd2
ShiiRochi <ShiiRochi@users.noreply.github.com>
2018-08-22 19:28:37 +0300

    Item Modal Finished
    + cancellation of new item creation
    + fix old value is not cleared after modal is closed
    + make Input controllable

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import ItemsList from "./ItemsList";
 import SortableItemsList from "./SortableItemsList";
 import HTML5Backend from "react-dnd-html5-backend";
 import { DragDropContext } from "react-dnd";
-import Switch from 'react-switch';
-import update from 'immutability-helper';
+import Switch from "react-switch";
+import update from "immutability-helper";
 import Target from "./Target";
 import ItemModal from "./ItemModal";
-import uuid from 'uuid/v1';
+import uniqueId from "lodash/uniqueId";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 
 const initialState = {
   mode: "sort", // delete or sorting
-  itemModal:{
-    visible: false,
+  itemModal: {
+    visible: false
   },
   data: [
     {
@@ -38,11 +38,7 @@ class App extends React.Component {
   state = initialState;
 
   handleElementDrop = id => {
-    if (this.state.mode === "delete") {
-      this.deleteElement(id);
-    } else if (this.state.mode === "sorting") {
-      // this.modeItem();
-    }
+    this.deleteElement(id);
   };
 
   deleteElement = id => {
@@ -65,63 +61,87 @@ class App extends React.Component {
 
   handleRefresh = () => {
     this.setState({
-      data: initialState.data,
+      data: initialState.data
     });
   };
 
   handleSwitchChange = (checked, event) => {
-    this.setState({ 
-      mode: checked ? 'sort' : 'delete',  
+    this.setState({
+      mode: checked ? "sort" : "delete"
     });
   };
 
-  handleItemCreate = (item) => {
-    this.setState(update(this.state, {
-      data: {
-        $push: [{ id: uuid(), ...item}]
-      }
-    }));
-  }
+  handleItemCreate = item => {
+    if (!item.content) {
+      return;
+    }
+    this.setState(
+      update(this.state, {
+        data: {
+          $push: [{ id: uniqueId("item_"), ...item }]
+        },
+        itemModal: {
+          $set: false
+        }
+      })
+    );
+  };
 
   handleItemCreateClick = () => {
     this.setState({
       itemModal: {
-        visible: true,
+        visible: true
       }
     });
-  }
+  };
+
+  handleItemModalCancelClick = () => {
+    this.setState({
+      itemModal: {
+        visible: false
+      }
+    });
+  };
 
   render() {
     const { data, mode, itemModal } = this.state;
 
-    const checked = mode === 'sort';
+    const checked = mode === "sort";
 
-    const modeTitle = mode === 'sort' ? 'Sortable List' : 'Removing Items';
+    const modeTitle = mode === "sort" ? "Sortable List" : "Removing Items";
 
     return (
       <div className="App">
         <h1>React DnD Example ("{modeTitle}")</h1>
         <h3>For mode changing use toggle below</h3>
-        <ItemModal 
+        <ItemModal
           visible={itemModal.visible}
           onSubmit={item => this.handleItemCreate(item)}
-         />
+          onCancel={this.handleItemModalCancelClick}
+        />
         <div className="App-inner">
           <div className="control-panel">
-            <button className="refresh-btn" onClick={this.handleRefresh}>Refresh</button>
-            <Switch
-              checked={checked}
-              onChange={this.handleSwitchChange}
-            />
+            <button className="refresh-btn" onClick={this.handleRefresh}>
+              Refresh
+            </button>
+            <Switch checked={checked} onChange={this.handleSwitchChange} />
           </div>
           {mode === "delete" && (
             <Fragment>
-              <ItemsList onCreate={this.handleItemCreateClick} onDrop={this.handleElementDrop} items={data} />
+              <ItemsList
+                onCreate={this.handleItemCreateClick}
+                onDrop={this.handleElementDrop}
+                items={data}
+              />
               <Target />
             </Fragment>
           )}
           {mode === "sort" && (
-            <SortableItemsList onCreate={this.handleItemCreateClick} onMove={this.moveItem} items={data} />
+            <SortableItemsList
+              onCreate={this.handleItemCreateClick}
+              onMove={this.moveItem}
+              items={data}
+            />
           )}
         </div>
       </div>
--- src/ItemModal/index.js
+++ src/ItemModal/index.js
@@ -1,55 +1,70 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { 
-    Modal, 
-    ModalBody, 
-    ModalFooter, 
-    ModalHeader,
-    Input,
-    Label,
-    Button,
- } from 'reactstrap';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import {
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  Input,
+  Label,
+  Button
+} from "reactstrap";
 
 class ItemModal extends Component {
-    state = {
-        content: '',
-    };
-    onSubmit = () => {
-        const { content } = this.state;
-        this.props.onSubmit({ content });
-    };
-    onCancel = () => {
-        this.props.onCancel();
-    };
-    handleItemChange = updateData => {
-        this.setState(updateData);
-    };
-    render() {
-        const {
-            visible,
-            content,
-        } = this.props;
-        return (
-            <Modal isOpen={visible}>
-                <ModalHeader>
-                    Add New Element
-                </ModalHeader>
-                <ModalBody>
-                    <Label>Element Text</Label>
-                    <Input onChange={e => this.handleItemChange({ content: e.target.value })} />
-                </ModalBody>
-                <ModalFooter>
-                    <Button color="primary" onClick={this.onSubmit}>Save</Button>
-                    <Button color="secondary" onClick={this.onCancel}>Cancel</Button>
-                </ModalFooter>
-            </Modal>
-        );
-    }
+  state = {
+    content: ""
+  };
+
+  onSubmit = () => {
+    const { content } = this.state;
+    this.props.onSubmit({ content });
+    this.clearValues();
+  };
+
+  onCancel = () => {
+    this.props.onCancel();
+    this.clearValues();
+  };
+
+  handleItemChange = updateData => {
+    this.setState(updateData);
+  };
+
+  clearValues = () => {
+    this.setState({
+      content: ""
+    });
+  };
+
+  render() {
+    const { visible } = this.props;
+    const { content } = this.state;
+    return (
+      <Modal isOpen={visible}>
+        <ModalHeader>Add New Element</ModalHeader>
+        <ModalBody>
+          <Label>Element Text</Label>
+          <Input
+            onChange={e => this.handleItemChange({ content: e.target.value })}
+            value={content}
+          />
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={this.onSubmit}>
+            Save
+          </Button>
+          <Button color="secondary" onClick={this.onCancel}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
 }
 
 ItemModal.propTypes = {
-    onSubmit: PropTypes.func,
-    onCancel: PropTypes.func,
+  onSubmit: PropTypes.func,
+  onCancel: PropTypes.func
 };
 
 export default ItemModal;
